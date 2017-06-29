31ca9e24cdc4d9dda9d15bc9fce65ce294f2d00a
haoliu <haoliu@nvidia.com>
2017-06-28 17:41:14 -0700

    a just working IndexExporter that exports _source for every live doc in a Lucene index

---
+++ src/main/java/repo/myjpa/elasticExport/Main.java
@@ -0,0 +1,15 @@
+package repo.myjpa.elasticExport;
+import java.io.IOException;
+
+/**
+ * Created by haoliu on 6/28/2017.
+ */
+public class Main {
+    public static void main(String[] argv) throws IOException {
+
+        System.out.println("start");
+        IndexExporter exporter = new IndexExporter(argv[0]);
+        exporter.export(System.out);
+
+    }
+}
